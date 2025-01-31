36c8ce281574eeda8aea1b78b3f63e456619171e
ChanvirappaPrachande <chanvirappap@gmail.com>
2025-01-31 22:49:36 +0530

    Update content.js

--- content.js
+++ content.js
@@ -42,7 +42,19 @@ function restoreScrollPosition() {
   });
 }
 
+
 window.addEventListener("load", () => {
   restoreScrollPosition();
   setInterval(saveScrollPosition, SAVE_INTERVAL);
 });
+
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "hidden") {
+    saveScrollPosition();
+  }
+});
+
+window.addEventListener("beforeunload", () => {
+  saveScrollPosition();
+});
+
